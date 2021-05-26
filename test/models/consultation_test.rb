1aed6adaf2e3ef8e6faf96ea30f80f3fc0d749bd
santdev404 <santdev404@gmail.com>
2021-05-26 06:20:51 -0500

    test Validatios

--- test/models/consultation_test.rb
+++ test/models/consultation_test.rb
@@ -4,4 +4,19 @@ class ConsultationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = users(:one)
+    @consultation = Consultation.new(disease: "ABC",user_id: @user.id)  
+    # @consultation = consultations(:one)
+  end
+
+  test "Consultation should be valid" do
+    assert @consultation.valid?
+  end
+
+  test "Consultation should not be Valid" do
+    @consultation.user_id = nil
+    refute @consultation.valid?
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,9 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+
+class ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+  include Devise::Test::IntegrationHelpers
+end

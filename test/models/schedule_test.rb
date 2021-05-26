05ed8db717970ae825b0fc6b9134924cdf088e1c
santdev404 <santdev404@gmail.com>
2021-05-26 06:20:51 -0500

    test Validatios

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -4,4 +4,20 @@ class ScheduleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = users(:one)
+    @consultation = consultations(:one)
+    @schedule = Schedule.new(user_id: @user.id, consultation_id: @consultation.id)  
+    # @consultation = consultations(:one)
+  end
+
+  test "Consultation should be valid" do
+    assert @schedule.valid?
+  end
+
+  test "Consultation should not be Valid" do
+    @schedule.user_id = nil
+    refute @schedule.valid?
+  end
 end
